ac70a85b8706d5590b1ee3bd724d7afea48a5736
IONELA VLADASEL <108732298+vladasel@users.noreply.github.com>
2022-08-11 10:01:24 +0200

    Add files via upload

---
+++ ProjectGradleDemo/src/test/java/com/ionela/gradle/ProjectGradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionela.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGradleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
