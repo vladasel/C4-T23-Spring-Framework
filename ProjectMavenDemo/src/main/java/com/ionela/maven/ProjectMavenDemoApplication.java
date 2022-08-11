39961e714dae9369ecfb701935809ac8d84c2fbd
IONELA VLADASEL <108732298+vladasel@users.noreply.github.com>
2022-08-11 10:01:57 +0200

    Add files via upload

---
+++ ProjectMavenDemo/src/main/java/com/ionela/maven/ProjectMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ionela.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMavenDemoApplication.class, args);
+	}
+
+}
---
+++ ProjectMavenDemo/src/test/java/com/ionela/maven/ProjectMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionela.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
